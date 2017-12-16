3a51ec8b712b4ab57233c83e463ee8319d00b63f
ANNA YANG <yanganna68@gmail.com>
2017-12-16 04:27:29 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,8 @@ def hit?
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
